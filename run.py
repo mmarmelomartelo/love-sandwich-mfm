494445bd010610263ae80096b271ad2e1338ea70
Marcelo  Montenegro <montenegromf@gmail.com>
2023-03-25 00:07:18 +0000

    Add validation to check the exactly 6 values of data is provided by the user

--- run.py
+++ run.py
@@ -8,23 +8,44 @@ SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
     "https://www.googleapis.com/auth/drive.file",
     "https://www.googleapis.com/auth/drive"
-    ]
+]
 
-CREDS = Credentials.from_service_account_file ("creds.json")
+CREDS = Credentials.from_service_account_file("creds.json")
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open("love_sandwiches")
 
 sales = SHEET.worksheet("sales")
 
+
 def get_sales_data():
     """
     Get the number of sandwiches sales from the user
     """
-print("Please enter the data sales from the last marked day")
-print("the data format should be 6 numbers separated by commas")
-print("Data example: 10, 20, 30, 40, 50, 60\n")
-data_str = input("Enter your data here: ")
-print(f"The data provided is {data_str}")
 
+
+    print("Please enter the data sales from the last marked day")
+    print("the data format should be 6 numbers separated by commas")
+    print("Data example: 10, 20, 30, 40, 50, 60\n")
+    data_str = input("Enter your data here: ")
+    print(f"The data provided is {data_str}")
+
+    sales_data = data_str.split(",")
+    validate_data(sales_data)
+
+def validate_data(values):
+    
+    """
+    Inside the try, coverts all string values into integers.
+    Raises ValuesError if strings cannot be converted into int,
+    or if there are not exactly 6 values
+    """
+    try:
+        if len(values) != 6:
+            raise ValueError(
+                f"Exactly 6 values require, you provided {len(values)}"
+            )
+    except ValueError as e:
+        print(f"Invalid data: {e}, please try again.\n")
 get_sales_data()
+
